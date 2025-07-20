4e8801998ebdbca6de6e90aad9cd5cee5977db2d
Delusionistic <140865207+Delusionistic@users.noreply.github.com>
2025-07-19 21:30:13 -0500

    UI Complete
    Alll UI is working

--- Assets/Level/SceneLoader.cs
+++ Assets/Level/SceneLoader.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using Unity.VisualScripting;
-using UnityEngine;
+﻿//using System;
+//using Unity.VisualScripting;
+//using UnityEngine;
 
-namespace Level
-{
-    public class SceneLoader : MonoBehaviour
-    {
-        public string LevelToLoad = "";
+//namespace Level
+//{
+//    public class SceneLoader : MonoBehaviour
+//    {
+//        public string LevelToLoad = "";
         
-        public void LoadLevel()
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(LevelToLoad);
-        }
+//        public void LoadLevel()
+//        {
+//            UnityEngine.SceneManagement.SceneManager.LoadScene(LevelToLoad);
+//        }
 
-        private void OnCollisionEnter2D(Collision2D other)
-        {
-            var player = other.gameObject.GetComponent<Player.PlayerController>();
-            if (player != null)
-            {
-                LoadLevel();
-            }
-        }
-    }
-}
+//        private void OnCollisionEnter2D(Collision2D other)
+//        {
+//            var player = other.gameObject.GetComponent<Player.PlayerController>();
+//            if (player != null)
+//            {
+//                LoadLevel();
+//            }
+//        }
+//    }
+//}
