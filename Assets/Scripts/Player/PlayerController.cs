29e86d320de523993e9ccfbf2413af6b0bc2bd5b
Delusionistic <140865207+Delusionistic@users.noreply.github.com>
2025-07-20 11:00:23 -0500

    Animations
    Added Animations, Updated Player Script

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,44 +5,78 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-
     public float jumpPower = 5f;
-    float horizontal;
 
+    float horizontal;
     bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-    // Update is called once per frame
+    [SerializeField] private Animator animator; // Drag Side_0's Animator here in Inspector
+
+    private bool isJumping = false;
+    private bool isGrounded = false;
+
+    void Start()
+    {
+        if (animator == null)
+        {
+            animator = GetComponentInChildren<Animator>();
+        }
+    }
+
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        // Always check if grounded and update jump state
+        bool groundedNow = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        if (groundedNow && !isGrounded)
+        {
+            isJumping = false;
+            animator.SetBool("isJumping", false);
+        }
+
+        isGrounded = groundedNow;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            isJumping = true;
+            animator.SetBool("isJumping", true);
         }
-        
+
         if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
-
         }
 
         Flip();
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.linearVelocity.x));
+        animator.SetFloat("yVelocity", rb.linearVelocity.y);
     }
 
     private bool IsGrounded()
     {
-       return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        bool grounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
+        if (grounded && isJumping)
+        {
+            isJumping = false;
+            animator.SetBool("isJumping", false);
+        }
+
+        return grounded;
     }
+
     private void Flip()
     {
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
